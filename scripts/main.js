b3886223360db89c045fd5b5dec9e8386ca729b2
Cooldownn <dphung291@gmail.com>
2019-06-18 23:30:54 +0700

    First UI

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,4 @@
 function checkUser() {
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
-    alert(email + " " + password);
-    window.location.href = 'homepage.html';
 }
