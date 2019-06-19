330b7e2e9ccdd58be66c5716f3406ef4edb7bc7b
Cooldownn <dphung291@gmail.com>
2019-06-19 22:26:34 +0700

    OK

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,64 @@
 function checkUser() {
-    // const email = document.getElementById('email').value;
-    // const password = document.getElementById('password').value;
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+
     const proxyurl = "https://cors-anywhere.herokuapp.com/";
-    const url = "https://mhrsystem.herokuapp.com/users"; // site that doesn’t send Access-Control-*
-    fetch(proxyurl + url) // https://cors-anywhere.herokuapp.com/https://mhrsystem.herokuapp.com
-    .then(response => response.text())
-    .then(contents => console.log(contents))
-    .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
+    const url = "https://mhrsystem.herokuapp.com/users/signin"; // site that doesn’t send Access-Control-*
+
+    var request = new XMLHttpRequest()
+    request.open('POST',proxyurl + url, true)
+    request.setRequestHeader('Content-Type', 'application/json')
+    request.onload = function() {
+        var data = JSON.parse(this.response)
+        if (request.status >= 200 && request.status < 400) {
+            if (data.code == "200") {
+                window.location = "homepage.html";
+            } else {
+                alert('Please check your email - password!');
+            }
+        }
+        else {
+            console.log('error');
+        }
+    }
+    request.send(JSON.stringify({
+        email: email,
+        password: password
+    }));
+}
+
+function signUp() {
+    var newEmail = document.getElementById('newemail').value;
+    var newPassword = document.getElementById('newpassword').value;
+    var confirmPassword = document.getElementById('confirmpassword').value;
+
+    if (newPassword != confirmPassword) {
+        alert('Your password does not match');
+    } else {
+        const proxyurl = "https://cors-anywhere.herokuapp.com/";
+        const url = "https://mhrsystem.herokuapp.com/users/signup"; // site that doesn’t send Access-Control-*
+        var request = new XMLHttpRequest()
+        request.open('POST',proxyurl + url, true)
+        request.setRequestHeader('Content-Type', 'application/json')
+        request.onload = function() {
+            var data = JSON.parse(this.response)
+            if (request.status >= 200 && request.status < 400) {
+                if (data.code == "200") {
+                    alert('Successfully created your account');
+                    window.location = "login.html";
+                } else {
+                    alert('Something went wrong! Please try again later');
+                }
+            }
+            else {
+                console.log('error');
+            }
+        }
+        request.send(JSON.stringify({
+            email: newEmail,
+            password: newPassword,
+            name: 'EAD',
+            role: 'employee'
+        }));
+    }
 }
