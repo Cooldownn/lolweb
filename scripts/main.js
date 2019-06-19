a1392432e495ce144cdb9e194143269f05b3c5ec
Cooldownn <dphung291@gmail.com>
2019-06-19 11:44:42 +0700

    Finish UI

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,10 @@
 function checkUser() {
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
+    // const email = document.getElementById('email').value;
+    // const password = document.getElementById('password').value;
+    const proxyurl = "https://cors-anywhere.herokuapp.com/";
+    const url = "https://mhrsystem.herokuapp.com/users"; // site that doesn’t send Access-Control-*
+    fetch(proxyurl + url) // https://cors-anywhere.herokuapp.com/https://mhrsystem.herokuapp.com
+    .then(response => response.text())
+    .then(contents => console.log(contents))
+    .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
 }
